92cb39b69e4aed3b4f6d1cf624251c1cc0a165ed
kochjar <timgfx@outlook.com>
2023-05-17 13:51:05 +0200

    Add project files.

---
+++ Ex33-Delegates/Bonuses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex33_Delegates
+{
+    public delegate double BonusProvider(double amount);
+    public class Bonuses
+    {
+        public static double TenPercent(double amount){ return amount * 0.10;}
+
+        public static double FlatTwoIfAmountMoreThanFive(double amount) 
+        {
+            if (amount > 5) return 2;
+            else return 0;
+        }
+
+    }
+}
---
+++ Ex33-Delegates/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex33_Delegates
+{
+    public class Order
+    {
+
+        public BonusProvider Bonus { get; set; }
+
+        public List<Product> products { get; set; }
+
+        public Order()
+        {
+            this.products = new List<Product>();
+        }
+
+        public void AddProduct(Product product) { products.Add(product);}
+
+        public double GetValueOfProducts() {
+            double sum = 0;
+            foreach(Product product in products) { sum += product.Value; }
+            return sum;
+        }
+
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+
+
+    }
+}
